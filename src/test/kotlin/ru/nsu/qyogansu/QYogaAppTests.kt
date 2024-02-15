9c968c0cf185e3f79b7bfc9838905324e9e205b2
Aleksey Zhidkov <me@azhidkov.pro>
2024-02-15 15:44:44 +0700

    Добавлен экшн проверки ветки перед мёржем

--- src/test/kotlin/ru/nsu/qyogansu/QYogaAppTests.kt
+++ src/test/kotlin/ru/nsu/qyogansu/QYogaAppTests.kt
@@ -3,7 +3,7 @@ package ru.nsu.qyogansu
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(classes = [TestQYogaNsuApplication::class])
 class QYogaAppTests {
 
 	@Test
