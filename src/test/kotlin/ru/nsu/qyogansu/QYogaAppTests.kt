56d23ae4b6d28471e466a42fc42071fc613358a7
Aleksey Zhidkov <me@azhidkov.pro>
2024-02-15 15:44:44 +0700

    Исправлен тест запуска приложения

--- src/test/kotlin/ru/nsu/qyogansu/QYogaAppTests.kt
+++ src/test/kotlin/ru/nsu/qyogansu/QYogaAppTests.kt
@@ -3,7 +3,7 @@ package ru.nsu.qyogansu
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(classes = [QYogaAppTests::class])
 class QYogaAppTests {
 
 	@Test
